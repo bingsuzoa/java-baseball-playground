c89cbf905bd414ec1eb2dbd0534b68b725578747
bingsuzoa <zxc_777@naver.com>
2025-01-17 17:21:49 +0900

    feat : 컴퓨터 랜덤정답생성

---
+++ src/main/java/com/baseballgame/Main.java
@@ -0,0 +1,12 @@
+package com.baseballgame;
+
+import com.baseballgame.computer.Computer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        Integer[] answer = computer.getAnswer();
+
+    }
+}
---
+++ src/main/java/com/baseballgame/computer/Computer.java
@@ -0,0 +1,32 @@
+package com.baseballgame.computer;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Computer {
+    private Integer[] answer = new Integer[3];
+
+
+    private Integer[] createAnswer() {
+        Set<Integer> DuplicationCheck = new HashSet<>();
+
+        while(DuplicationCheck.size() < 3) {
+            DuplicationCheck.add(createRandomNumber());
+        }
+
+        Integer[] answer = DuplicationCheck.toArray(new Integer[0]);
+        return answer;
+    }
+
+    private int createRandomNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(9) + 1;
+        return randomNumber;
+    }
+
+    public Integer[] getAnswer() {
+        answer = createAnswer();
+        return answer;
+    }
+}
